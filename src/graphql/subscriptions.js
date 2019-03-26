d51e18febcf4238f2a5db1c51e2778e28a8e928b
Shufan Kang <shufakan@amazon.com>
2019-03-26 23:14:55 +0000

    add code gen

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,168 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const onCreateCreditLog = `subscription OnCreateCreditLog {
+  onCreateCreditLog {
+    id
+    item {
+      id
+      name
+      link
+      imageUrl
+      price
+      type
+      CreditLogs {
+        nextToken
+      }
+    }
+    user {
+      user
+      creditLogs {
+        nextToken
+      }
+    }
+    creditChange
+  }
+}
+`;
+export const onUpdateCreditLog = `subscription OnUpdateCreditLog {
+  onUpdateCreditLog {
+    id
+    item {
+      id
+      name
+      link
+      imageUrl
+      price
+      type
+      CreditLogs {
+        nextToken
+      }
+    }
+    user {
+      user
+      creditLogs {
+        nextToken
+      }
+    }
+    creditChange
+  }
+}
+`;
+export const onDeleteCreditLog = `subscription OnDeleteCreditLog {
+  onDeleteCreditLog {
+    id
+    item {
+      id
+      name
+      link
+      imageUrl
+      price
+      type
+      CreditLogs {
+        nextToken
+      }
+    }
+    user {
+      user
+      creditLogs {
+        nextToken
+      }
+    }
+    creditChange
+  }
+}
+`;
+export const onCreateItem = `subscription OnCreateItem {
+  onCreateItem {
+    id
+    name
+    link
+    imageUrl
+    price
+    type
+    CreditLogs {
+      items {
+        id
+        creditChange
+      }
+      nextToken
+    }
+  }
+}
+`;
+export const onUpdateItem = `subscription OnUpdateItem {
+  onUpdateItem {
+    id
+    name
+    link
+    imageUrl
+    price
+    type
+    CreditLogs {
+      items {
+        id
+        creditChange
+      }
+      nextToken
+    }
+  }
+}
+`;
+export const onDeleteItem = `subscription OnDeleteItem {
+  onDeleteItem {
+    id
+    name
+    link
+    imageUrl
+    price
+    type
+    CreditLogs {
+      items {
+        id
+        creditChange
+      }
+      nextToken
+    }
+  }
+}
+`;
+export const onCreateUser = `subscription OnCreateUser {
+  onCreateUser {
+    user
+    creditLogs {
+      items {
+        id
+        creditChange
+      }
+      nextToken
+    }
+  }
+}
+`;
+export const onUpdateUser = `subscription OnUpdateUser {
+  onUpdateUser {
+    user
+    creditLogs {
+      items {
+        id
+        creditChange
+      }
+      nextToken
+    }
+  }
+}
+`;
+export const onDeleteUser = `subscription OnDeleteUser {
+  onDeleteUser {
+    user
+    creditLogs {
+      items {
+        id
+        creditChange
+      }
+      nextToken
+    }
+  }
+}
+`;
